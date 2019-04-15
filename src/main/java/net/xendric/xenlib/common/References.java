378b519e3243746a877a2339ea7d2a29d11dc554
MCE626 <Contact.MCE626@gmail.com>
2019-04-14 20:44:27 -0500

    modinfo stuff, update url fix

--- src/main/java/net/xendric/xenlib/common/References.java
+++ src/main/java/net/xendric/xenlib/common/References.java
@@ -4,7 +4,7 @@ public class References {
 	public static final String MODID = "xenlib";
 	public static final String NAME = "XenLib";
 	public static final String VERSION = "1.12.2-1.0.0.0";
-	public static final String UPDATE = "https://github.com/XendricInteractive/XenLib/blob/master/update.json";
+	public static final String UPDATE = "https://raw.githubusercontent.com/XendricInteractive/XenLib/master/update.json";
 	public static final String CLIENT_PROXY_CLASS = "net.xendric.client.core.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "net.xendric.common.core.proxy.CommonProxy";
 }
