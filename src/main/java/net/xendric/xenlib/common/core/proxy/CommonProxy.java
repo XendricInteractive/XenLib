d9d9e78a549f1e86528b17292b6fb64d311411c8
MCE626 <Contact.MCE626@gmail.com>
2019-08-17 17:33:01 -0500

    Added BlockFluidBase and FluidBase.

--- src/main/java/net/xendric/xenlib/common/core/proxy/CommonProxy.java
+++ src/main/java/net/xendric/xenlib/common/core/proxy/CommonProxy.java
@@ -1,5 +1,15 @@
 package net.xendric.xenlib.common.core.proxy;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockLiquid;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
@@ -8,5 +18,69 @@ public class CommonProxy {
 	}
 
 	public void init(FMLInitializationEvent e) {
+	}
+
+	/**
+	 * For fluids to move entities
+	 * 
+	 * @param entity
+	 * @param mat
+	 * @return
+	 */
+	public boolean handleMaterialAcceleration(Entity entity, Material mat) {
+		World world = entity.world;
+		AxisAlignedBB AABB = entity.getEntityBoundingBox().grow(0.0D, -0.4000000059604645D, 0.0D).shrink(0.001D);
+
+		int aaXMin = MathHelper.floor(AABB.minX);
+		int aaXMax = MathHelper.ceil(AABB.maxX);
+		int aaYMin = MathHelper.floor(AABB.minY);
+		int aaYMax = MathHelper.ceil(AABB.maxY);
+		int aaZMin = MathHelper.floor(AABB.minZ);
+		int aaZMax = MathHelper.ceil(AABB.maxZ);
+
+		boolean successful = false;
+		Vec3d vec = Vec3d.ZERO;
+		BlockPos.PooledMutableBlockPos poolPos = BlockPos.PooledMutableBlockPos.retain();
+
+		for (int currentAAX = aaXMin; currentAAX < aaXMax; ++currentAAX) {
+			for (int currentAAY = aaYMin; currentAAY < aaYMax; ++currentAAY) {
+				for (int currentAAZ = aaZMin; currentAAZ < aaZMax; ++currentAAZ) {
+					poolPos.setPos(currentAAX, currentAAY, currentAAZ);
+					IBlockState state = world.getBlockState(poolPos);
+					Block block = state.getBlock();
+
+					Boolean result = block.isEntityInsideMaterial(world, poolPos, state, entity, aaYMax, mat, false);
+					if (result != null && result == true) {
+						successful = true;
+						vec = block.modifyAcceleration(world, poolPos, entity, vec);
+						continue;
+					} else if (result != null && result == false)
+						continue;
+
+					if (state.getMaterial() == mat) {
+						double newAAY = currentAAY + 1
+								- BlockLiquid.getLiquidHeightPercent(state.getValue(BlockLiquid.LEVEL).intValue());
+
+						if (aaYMax >= newAAY) {
+							successful = true;
+							vec = block.modifyAcceleration(world, poolPos, entity, vec);
+						}
+					}
+				}
+			}
+		}
+
+		poolPos.release();
+
+		if (vec.lengthVector() > 0.0D && entity.isPushedByWater()) {
+			vec = vec.normalize();
+			double addMotion = 0.014D;
+			entity.motionX += vec.x * addMotion;
+			entity.motionY += vec.y * addMotion;
+			entity.motionZ += vec.z * addMotion;
+		}
+
+		entity.fallDistance = 0.0F;
+		return successful;
 	}
 }
